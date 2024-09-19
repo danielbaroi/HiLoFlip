85dd9d923410d704dc34ab86a10e6248b5e5a926
Daniel Baroi <danielbaroi@cedarville.edu>
2024-09-19 13:31:58 -0400

    Initial Commit

---
+++ HiLoFlip/HiLoFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipApp.swift
+//  HiLoFlip
+//
+//  Created by Daniel Baroi on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HiLoFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HiLoFlipTests/HiLoFlipTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipTests.swift
+//  HiLoFlipTests
+//
+//  Created by Daniel Baroi on 9/19/24.
+//
+
+import Testing
+@testable import HiLoFlip
+
+struct HiLoFlipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
